97d775715bfb0bf1a5b7099f19d66258f14d63c6
Shaedys <shaedys@gmail.com>
2014-06-25 10:33:50 +0200

    Fixed checkeraux and AliaTypeException

--- src/alia/AliaTypeException.java
+++ src/alia/AliaTypeException.java
@@ -7,19 +7,14 @@ public class AliaTypeException extends AliaException {
 	private String msg;
 	private static final long serialVersionUID = 1L;
     // Ctor which only requires the error message to be printed.
-	public AliaException(String msg) {
-        super();
-        this.msg = msg;
+	public AliaTypeException(String msg) {
+        super(msg);
     }
 
     // Ctor that takes a node of the AST tree (i.e. IDENTIFIER) and
     // the error message to build a more informative error message.
-    public AliaException(Tree tree, String msg) {
-        super();
-        this.msg = tree.getText() +
-                " (" + tree.getLine() +
-                ":" + tree.getCharPositionInLine() +
-                ") " + msg;
+    public AliaTypeException(Tree tree, String msg) {
+        super(tree,msg);
     }
 
     @Override
--- src/alia/CheckerAux.java
+++ src/alia/CheckerAux.java
@@ -7,7 +7,7 @@ import alia.types.*;
 public abstract class CheckerAux {
 
 	
-	protected void checkBoolType (_Type a){
+	protected void checkBoolType (_Type a) throws AliaTypeException{
 		if (!a.equals(new _Bool())){
 			throw new AliaTypeException("Type exception, expression is not a boolean.");
 		}
@@ -26,19 +26,19 @@ public abstract class CheckerAux {
 		}
 	}
 	
-	protected void checkEqualType(_Type a, _Type b){
+	protected void checkEqualType(_Type a, _Type b) throws AliaTypeException{
 		if(!a.equals(b)){
-			throw new AliaTypeException("Type " + a.toString() + " is not equal to " + b.toString() ".");
+			throw new AliaTypeException("Type " + a.toString() + " is not equal to " + b.toString() + ".");
 		}
 	}
 	
-	protected void checkMathType(_Type a, _Type b){
+	protected void checkMathType(_Type a, _Type b) throws AliaTypeException{
 		if(!a.equals(new _Int()) || !b.equals(new _Int())){
 			throw new AliaTypeException("Math expressions require type Int");
 		}
 	}
 	
-	protected void checkNotVoid(_Type a){
+	protected void checkNotVoid(_Type a) throws AliaTypeException{
 		if(!a.equals(new _Void())){
 			throw new AliaTypeException("Not type void.");
 		}
