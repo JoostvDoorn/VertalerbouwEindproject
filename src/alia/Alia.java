2320c26ce092503fc00a79dad09dae23eeee5ffe
Joost van Doorn <joost.van.doorn@gmail.com>
2014-07-09 13:31:46 +0200

    Testresults updated

--- src/alia/Alia.java
+++ src/alia/Alia.java
@@ -166,9 +166,6 @@ public class Alia {
 
         }
         catch (RecognitionException e) {
-            System.err.print("ERROR: recognition exception thrown by compiler: ");
-            System.err.println(e.getMessage());
-            e.printStackTrace();
         } catch (Exception e) {
             System.err.print("ERROR: uncaught exception thrown by compiler: ");
             System.err.println(e.getMessage());
